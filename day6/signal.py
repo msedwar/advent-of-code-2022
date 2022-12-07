24ef7f86a20f047f666d5ace0bcd9a44d4886980
Matthew Edwards <msedwar@umich.edu>
2022-12-06 17:38:44 -0800

    Day 6, part 2 completed

--- day6/signal.py
+++ day6/signal.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
+MESSAGE_SIZE = 14
+
 def main():
     data = input("? ")
-    assert len(data) >= 4
+    assert len(data) >= MESSAGE_SIZE
     
-    for index in range(0, len(data) - 3):
-        buf = data[index:index + 4]
-        if len(set(buf)) == 4:
+    for index in range(0, len(data) - MESSAGE_SIZE + 1):
+        buf = data[index:index + MESSAGE_SIZE]
+        if len(set(buf)) == MESSAGE_SIZE:
             break
 
-    print(f"Signal position: {index + 4}")
+    print(f"Signal position: {index + MESSAGE_SIZE}")
 
 
 if __name__ == "__main__":

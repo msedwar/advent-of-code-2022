896eb35917073439827e4c5a4ab79a071c27c106
Matthew Edwards <msedwar@umich.edu>
2022-12-06 16:44:43 -0800

    Day 4, part 2 completed

--- day4/assignments.py
+++ day4/assignments.py
@@ -2,12 +2,22 @@
 
 import sys
 
-def range_contains(range1, range2):
+def range_fully_contains(range1, range2):
     range1_lower = range1[0] <= range2[0]
     range1_higher = range1[1] >= range2[1]
     range2_lower = range2[0] <= range1[0]
     range2_higher = range2[1] >= range1[1]
     return range1_lower == range1_higher or range2_lower == range2_higher # Either range1 contains range 2, or vice versa.
+
+def range_overlaps(range1, range2):
+    if range1[0] <= range2[0]:
+        lower_range = range1
+        higher_range = range2
+    else:
+        lower_range = range2
+        higher_range = range1
+    
+    return lower_range[1] >= higher_range[0]
 
 def string_to_range(s):
     in1, in2 = s.split("-")
@@ -20,10 +30,10 @@ def main():
         range_in1, range_in2 = line.split(",")
         range1 = string_to_range(range_in1)
         range2 = string_to_range(range_in2)
-        if range_contains(range1, range2):
+        if range_overlaps(range1, range2):
             total += 1
     
-    print(f"{total} assignment ranges fully overlap.")
+    print(f"{total} assignment ranges overlap.")
 
 
 if __name__ == "__main__":

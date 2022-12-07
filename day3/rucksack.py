7de8e8b1e4a392af83d2f2f2143f83414ef94a78
Matthew Edwards <msedwar@umich.edu>
2022-12-06 16:19:50 -0800

    Day 3, part 2 completed

--- day3/rucksack.py
+++ day3/rucksack.py
@@ -9,21 +9,28 @@ def get_score(letter):
     else:
         return ascii_value - ord("A") + 27
 
-def find_common(comp1, comp2):
-    letters = set(comp1)
-    for c in comp2:
-        if c in letters:
-            return c
-    return None
+def find_common(comp_list):
+    assert len(comp_list) >= 2
+    letters = set(comp_list[0])
+    for comp in comp_list[1:]:
+        comp_set = set(comp)
+        # Could also do this manually with a map and element counts.
+        letters = letters.intersection(comp_set)
+    
+    assert len(letters) == 1
+    return letters.pop()
 
 def main():
     total = 0
+    lines = []
     for line in sys.stdin:
         line = line.strip()
-        half = len(line)//2
-        comp1, comp2 = line[:half], line[half:]
-        common = find_common(comp1, comp2)
-        total += get_score(common)
+        lines.append(line)
+
+        if len(lines) == 3:
+            common = find_common(lines)
+            total += get_score(common)
+            lines.clear()
     
     print(f"Total priorities of: {total}")
 
